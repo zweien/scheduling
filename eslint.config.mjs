8b450e113a30ad4d811ee1b9c8c3f0d7cce993d0
zweien <278954153@qq.com>
2026-03-16 17:22:09 +0800

    chore(lint): ignore local worktrees

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,7 @@ const eslintConfig = defineConfig([
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
+    ".worktrees/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
